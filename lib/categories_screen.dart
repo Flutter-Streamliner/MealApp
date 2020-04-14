44bca6667c68ee463c3093604f646ef90bea33ae
Tiger <thisisstreamliner@gmail.com>
2020-04-14 12:58:04 +0300

    creating category item, adding linear gradient background

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/category_item.dart';
+import 'package:meal_app/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[
-
-      ],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body : GridView(
+        children: DUMMY_CATEGORIES.map((cat) => CategoryItem(title: cat.title, color: cat.color,)).toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20
+        ),
       ),
     );
   }
