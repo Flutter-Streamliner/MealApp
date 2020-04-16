e4d5874baf2f1db8ef4fab2d8d22e1bd56de908f
Tiger <thisisstreamliner@gmail.com>
2020-04-16 10:35:28 +0300

    Creating top tab controller

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,19 +5,14 @@ import 'package:meal_app/dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DeliMeals'),
-      ),
-      body : GridView(
-        padding: const EdgeInsets.all(25),
-        children: DUMMY_CATEGORIES.map((cat) => CategoryItem(id: cat.id, title: cat.title, color: cat.color,)).toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20
-        ),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      children: DUMMY_CATEGORIES.map((cat) => CategoryItem(id: cat.id, title: cat.title, color: cat.color,)).toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20
       ),
     );
   }
